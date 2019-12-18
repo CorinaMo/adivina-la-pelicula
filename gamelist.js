8f4ff7ce04482013daee0561dd034ba58871af2a
Cora <crista.majere@gmail.com>
2019-12-18 16:54:04 +0100

    adding my files to my repository

---
+++ gamelist.js
@@ -0,0 +1,308 @@
+list = [
+    {
+        frase:"A veces es la gente de la que nadie espera nada la que hace cosas que nadie puede imaginar",
+        pelicula:"DESCIFRANDO ENIGMA"
+    },
+    {
+        frase:"Todo tiene que terminar en algún momento. De lo contrario, nada hubiera comenzado",
+        pelicula:"DOCTOR WHO"
+    },
+    {
+        frase:"Lo mejor que puedo hacer con la muerte es tratar de aprovechar la vida",
+        pelicula:"HACIA RUTAS SALVAJES"
+    },
+    {
+        frase:"A pesar de lo que les digan, las palabras y las ideas pueden cambiar el mundo",
+        pelicula:"EL CLUB DE LOS POETAS MUERTOS"
+    },
+    {
+        frase:"Lo que posees acabará poseyéndote",
+        pelicula:"EL CLUB DE LA LUCHA"
+    },
+    {
+        frase:"Que seas una personalidad no significa que tengas personalidad",
+        pelicula:"PULP FICTION"
+    },
+    {
+        frase:"No me acuerdo de olvidarte",
+        pelicula:"MEMENTO"
+    },
+    {
+        frase:"Ahora puedo decirte que tomé la decisión correcta, sin embargo, no hay un día que pase sin arrepentirme de no haber tomado una opción diferente",
+        pelicula:"SEVEN"
+    },
+    {
+        frase:"Hay tres maneras de hacer las cosas: la correcta, la incorrecta y la mía",
+        pelicula:"CASINO"
+    },
+    {
+        frase:"¿Es dura la experiencia de vivir con miedo, verdad? En eso consiste ser esclavo",
+        pelicula:"BLADE RUNNER"
+    },
+    {
+        frase:"He cruzado océanos de tiempo para encontrarte",
+        pelicula:"DRACULA"
+    },
+    {
+        frase:"Sin ti las emociones de hoy solo serían las envolturas muertas de las del ayer",
+        pelicula:"AMELIE"
+    },
+    {
+        frase:"Nuestro nombre no importa, se nos conoce por nuestros actos",
+        pelicula:"BATMAN VUELVE"
+    },
+    {
+        frase:"Los dioses nos envidian porque cada instante nuestro podría ser el último",
+        pelicula:"TROYA"
+    },
+    {
+        frase:"El artista miente para mostrar la verdad, el político para ocultarla",
+        pelicula:"V DE VENDETTA"
+    },
+    {
+        frase:"No existen preguntas sin respuesta, solo preguntas mal formuladas",
+        pelicula:"MATRIX"
+    },
+    {
+        frase:"Todos los hombre mueren, pero no todos realmente viven",
+        pelicula:"BRAVEHEART"
+    },
+    {
+        frase:"Sí, el pasado puede doler. Pero como yo lo veo puedes huir o aprender de él",
+        pelicula:"EL REY LEON"
+    },
+    {
+        frase:"Hay dos tipos de personas: las que consiguen lo que quieren y las que no se atreven a conseguir lo que quieren",
+        pelicula:"CIUDADANO KANE"
+    },
+    {
+        frase:"El odio es un lastre, la vida es demasiado corta para estar siempre cabreado",
+        pelicula:"AMERICAN HISTORY X"
+    },
+    {
+        frase:"Yo solo puedo mostrarte la puerta, tú eres quien la tiene que atravesar",
+        pelicula:"MATRIX"
+    },
+    {
+        frase:"Pensamos demasiado y sentimos muy poco…",
+        pelicula:"EL GRAN DICTADOR"
+    },
+    {
+        frase:"Qué pacífica sería la vida sin amor, que segura y tranquila… y qué insulsa",
+        pelicula:"EL NOMBRE DE LA ROSA"
+    },
+    {
+        frase:"Hablar constantemente no implica que nos estemos comunicando",
+        pelicula:"OLVIDATE DE MI"
+    },
+    {
+        frase:"No se ve bien más que con el corazón, lo esencial es invisible para los ojos",
+        pelicula:"FORREST GUMP"
+    },
+    {
+        frase:"Porque tendrás momentos malos, pero ellos te llevarán a ver las cosas buenas que antes no prestabas atención",
+        pelicula:"EL INDOMABLE WILL HUNTING"
+    },
+    {
+        frase:"La duda puede ser un vínculo tan poderoso y sólido como la certeza",
+        pelicula:"LA DUDA"
+    },
+    {
+        frase:"Me llamo Íñigo Montoya, tú mataste a mi padre, preparate a morir. Me llamo Íñigo Montoya, tú mataste a mi padre...",
+        pelicula:"LA PRINCESA PROMETIDA"
+    },
+    {
+        frase:"Yo… he visto cosas que vosotros no creeríais: Atacar naves en llamas más allá de Orión. He visto rayos C brillar en la oscuridad cerca de la Puerta de Tannhäuser. Todos esos momentos se perderán en el tiempo, como lágrimas en la lluvia. Es hora de morir",
+        pelicula:"BLADE RUNNER"
+    },
+    {
+        frase:"Nos dicen que recordemos a los ideales, no al hombre, porque con un hombre se puede acabar. Pueden detenerle, pueden matarle, pueden olvidarle, pero cuatrocientos años más tarde los ideales aún pueden seguir cambiando el mundo",
+        pelicula:"V DE VENDETTA"
+    },
+    {
+        frase:"El pueblo no debería temer al gobierno, el gobierno debería temer al pueblo",
+        pelicula:"V DE VENDETTA"
+    },
+    {
+        frase:"Nadie es perfecto...",
+        pelicula:"CON FALDAS Y A LO LOCO"
+    },
+    {
+        frase:"Buenos días… y por si no volvemos a vernos: buenos días, buenas tardes y buenas noches",
+        pelicula:"EL SHOW DE TRUMAN"
+    },
+    {
+        frase:"Los que podáis caminar podéis iros, pero dejad vuestros miembros cercenados, ahora son míos",
+        pelicula:"KILL BILL"
+    },
+    {
+        frase:"O mueres como un héroe o vives lo suficiente para verte convertido en un villano ",
+        pelicula:"BATMAN EL CABALLERO OSCURO"
+    },
+    {
+        frase:"No puedes cambiar lo que es alguien sin destruir lo que fue",
+        pelicula:"EL EFECTO MARIPOSA"
+    },
+    {
+        frase:"Dar cera... pulir cera...",
+        pelicula:"KARATE KID"
+    },
+    {
+        frase:"La destrucción es una forma de creación",
+        pelicula:"DONNIE DARKO"
+    },
+    {
+        frase:"Le haré una oferta que no podrá rechazar",
+        pelicula:"EL PADRINO"
+    },
+    {
+        frase:"Hazlo o no lo hagas, pero no lo intentes",
+        pelicula:"EL IMPERIO CONTRAATACA"
+    },
+    {
+        frase:"¡Nos estábamos tomando un descanso!",
+        pelicula:"FRIENDS"
+    },
+    {
+        frase:"¿He sido yo?",
+        pelicula:"COSAS DE CASA"
+    },
+    {
+        frase:"Todo el mundo miente",
+        pelicula:"HOUSE"
+    },
+    {
+        frase:"Me encanta que los planes salgan bien",
+        pelicula:"EL EQUIPO A"
+    },
+    {
+        frase:"¡Zas, en toda la boca!",
+        pelicula:"PADRE DE FAMILIA"
+    },
+    {
+        frase:"¡Multiplícate por cero!",
+        pelicula:"LOS SIMPSON"
+    },
+    {
+        frase:"Bazinga!",
+        pelicula:"THE BIG BANG THEORY"
+    },
+    {
+        frase:"La verdad está ahí fuera",
+        pelicula:"EXPEDIENTE X"
+    },
+    {
+        frase:"No puedes jugar a ser Dios sin conocer bien al demonio",
+        pelicula:"WESTWORLD"
+    },
+    {
+        frase:"Lo hice por mí. Me gustaba. Era bueno haciéndolo y de verdad… me sentía vivo",
+        pelicula:"BREAKING BAD"
+    },
+    {
+        frase:"No sé si quiero vivir, si debo hacerlo, o si sólo es un hábito",
+        pelicula:"THE WALKING DEAD"
+    },
+    {
+        frase:"No existen secretos en la vida. Solo verdades escondidas que viven debajo de la superficie",
+        pelicula:"DEXTER"
+    },
+    {
+        frase:"El caos no es un foso, es una escalera. Muchos intentan subirla y fracasan",
+        pelicula:"JUEGO DE TRONOS"
+    },
+    {
+        frase:"No creas nada de lo que oigas y ni la mitad de lo que veas",
+        pelicula:"LOS SOPRANO"
+    },
+    {
+        frase:"Lo que llamas amor fue inventado por tipos como yo para vender medias",
+        pelicula:"MAD MEN"
+    },
+    {
+        frase:"No hay santos en el reino animal. Sólo desayuno y cena",
+        pelicula:"FARGO"
+    },
+    {
+        frase:"Si nadie te odia, estás haciendo algo mal",
+        pelicula:"HOUSE"
+    },
+    {
+        frase:"No puedes diseñar tu vida como si fuera un edificio, basta con que la vivas y se va diseñando sola",
+        pelicula:"COMO CONOCI A VUESTRA MADRE"
+    },
+    {
+        frase:"¿El amor está en el aire? Erróneo. El nitrógeno, el oxígeno y el dióxido de carbono están en el aire",
+        pelicula:"THE BIG BANG THEORY"
+    },
+    {
+        frase:"No puedes jugar a Dios y lavarte las manos de las cosas que has creado",
+        pelicula:"BATTLESTAR GALACTICA"
+    },
+    {
+        frase:"Cuando abres tu mente a lo imposible, a veces encuentras la verdad",
+        pelicula:"FRINGE"
+    },
+    {
+        frase:"Sigue el camino de baldosas amarillas",
+        pelicula:"EL MAGO DE OZ"
+    },
+    {
+        frase:"Amar significa no tener que decir nunca lo siento",
+        pelicula:"LOVE STORY"
+    },
+    {
+        frase:"Recuerden, recuerden el 5 de noviembre. Conspiración, pólvora y traición. No veo la demora y siempre es la hora de evocarla sin dilación",
+        pelicula:"V DE VENDETTA"
+    },
+    {
+        frase:"'Yipikayei' hijo de puta",
+        pelicula:"LA JUNGLA DE CRISTAL"
+    },
+    {
+        frase:"En ocasiones veo muertos",
+        pelicula:"EL SEXTO SENTIDO"
+    },
+    {
+        frase:"Abogado, abogado, ¿estás ahí? Abogado, sal ratita, quiero verte la colita",
+        pelicula:"EL CABO DEL MIEDO"
+    },
+    {
+        frase:"Qué delicia oler napalm por la mañana",
+        pelicula:"APOCALYPSE NOW"
+    },
+    {
+        frase:"Todos esos momentos se perderán en el tiempo como lágrimas en la lluvia. Es hora de morir",
+        pelicula:"BLADE RUNNER"
+    },
+    {
+        frase:"No hay cuchara",
+        pelicula:"MATRIX"
+    },
+    {
+        frase:"Pijus Magníficus",
+        pelicula:"LA VIDA DE BRIAN"
+    },
+    {
+        frase:"Que la fuerza te acompañe",
+        pelicula:"STAR WARS"
+    },
+    {
+        frase:"Esto va a ser LEGEN... espera... ¡DARIO!",
+        pelicula:"COMO CONOCI A VUESTRA MADRE"
+    },
+    {
+        frase:"Tu falta de fe me resulta molesta",
+        pelicula:"STAR WARS"
+    },
+
+
+    //PLANTILLA
+   /* 
+    {
+        frase:"",
+        pelicula:""
+    },
+
+    */
+]
---
+++ guessThe.js
@@ -0,0 +1,75 @@
+let listInUse = list;
+let pista = document.getElementById('pista');
+let peli = document.getElementById('peli');
+let line = "";
+let frase = "";
+let pelicula = "";
+let errores = 0;
+let partidasGanadas = 0;
+let readErrors = document.getElementById('errores');
+let readWins = document.getElementById('ganadas');
+let playSound = document.getElementById('keySound');
+startGame();
+
+//ESCOGE FRASE + PELI/SERIE, ENCRIPTA Y DEVUELVE
+function startGame(){
+    
+    let elementoLista = listInUse[Math.floor(Math.random() * listInUse.length)];
+    frase = elementoLista.frase.valueOf();
+    pelicula = elementoLista.pelicula.valueOf();
+
+    console.log('frase: ', frase, ' pelicula: ', pelicula);     ////////////
+    pista.innerHTML = `<h2><b> ${frase} </b></h2> <br/>`;
+    let peliArray = (pelicula.split(" "));
+
+    let peliEnc = peliArray.map(peliStr =>{
+        return peliStr.replace(/[A-Z]/g, ("_"));
+    });
+    line = (peliEnc.toString()).replace(/,/g, " ");
+    peli.innerHTML = `<b> ${line} </b>`
+}
+
+// ESCUCHA EVENTO DE KEYBOARD Y RESPONDE
+function keyUse(e){
+    let letraKey = (`${e.key}`).toUpperCase();
+    playSound.play();
+    let newLine = "";
+    let letter = line.split("");
+    
+    // REEMPLAZO DE "-" POR LETRA ORIGINAL SI EXISTE, SI NO EXISTE SUMARÁ 1 ERROR.
+    for(let i = 0; i < pelicula.length; i++){
+        if(pelicula[i] == letraKey){
+            newLine += letraKey;
+        }else{
+            newLine += letter[i];
+        }
+        line = newLine;
+        peli.innerHTML = `<b> ${line} </b>`;       
+    }   
+    if(pelicula.indexOf(letraKey) === -1){
+        errores += 1;
+        readErrors.innerHTML = `<b>Errores: ${errores} </b>`
+        if(errores === 8){
+            alert(`¡PERDISTE ESTA VEZ! Has ganado ${partidasGanadas} vez/veces.`);
+            location.reload();
+        }
+    }
+    setTimeout(go, 1000);
+    function go(){checkWin(line)};
+}
+
+// CHECK SI SE HA COMPLETADO LA FRASE
+function checkWin(l){
+    
+    if(l.indexOf("_") === -1){
+        alert(`¡Has ganado!`);
+        partidasGanadas += 1;
+        errores = 0;
+        readErrors.innerHTML = `<b>Errores: ${errores} </b>`
+        readWins.innerHTML = `<b>Partidas ganadas: ${partidasGanadas} </b>`
+        startGame();
+    }else{}    
+}
+ 
+//LISTENERS
+window.addEventListener('keydown', keyUse);
