eacb8df143f0d69919a9596cf5b18be50409e7c8
Cora <crista.majere@gmail.com>
2019-12-20 18:06:19 +0100

    commit

--- guessThe.js
+++ guessThe.js
@@ -28,7 +28,7 @@ $(function(){
     }    
 
     //POR CADA TECLA ALMACENADA EN TECCC, CUANDO SE HAGA CLIC, 
-    //DEVUELVE EL VALOR DE LA TECLA A LA FUNCIÓN REEMPLAZAR (DE GuessThe.js)   
+    //DEVUELVE EL VALOR DE LA TECLA A LA FUNCIÓN REEMPLAZAR  
     for( t of teccc){
         let i = teccc.indexOf(t);
         $(teccc[i]).click(function(){
